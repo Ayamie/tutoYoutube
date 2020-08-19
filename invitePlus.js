e3d292662f6d0a501e191583d060704f1fa83029
jordanoffshl <69113217+jordanoffshl@users.noreply.github.com>
2020-08-19 18:10:29 -0400

    Update invitePlus.js

--- invitePlus.js
+++ invitePlus.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 client.on("ready", message => {
-    console.log('salut je suis prêt');
-
+    console.log('salut je suis prêt');
+    client.user.setActivity('<message>',{ type : 'STREAMING'});
 });
 
 client.on("message", message => {
@@ -12,12 +12,30 @@ client.on("message", message => {
     }
 })
 
-const { prefix, token } = require('./config.json'); 
-const client = new Discord.Client(); 
-client.commands = new Discord.Collection(); 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); 
-for (const file of commandFiles) { 	
-const command = require(`./commands/${file}`); 
-client.commands.set(command.name, command); }
+// at the top of your file
+const Discord = require('discord.js');
 
-client.login('');
+// inside a command, event listener, etc.
+const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+channel.send(exampleEmbed);
+
+})
+
+client.login(process.env.TOKEN);
